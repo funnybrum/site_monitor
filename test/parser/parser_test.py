9b6cc712a28645bee02b1bc1a66a4b4e30840528
brum <funnybrum@gmail.com>
2017-10-06 08:33:29 +0300

    Adding a missing unit test for the case when there is no element for some of the xpaths. The test verifies that in case of missing image no error is thrown and that the image is set to None.

--- test/parser/parser_test.py
+++ test/parser/parser_test.py
@@ -20,6 +20,26 @@ class ParserTest(TestCase):
         parser = Parser(holmes_cfg.sites[0], holmes_cfg.headers)
 
         items = parser.process()
-        self.assertEqual(72, len(items))
+        self.assertEqual(1, len(items))
 
-    # TODO -> add tests for items with missing attributes.
+    @my_vcr.use_cassette('test_parser_holmes_bg_with_missing_elements.yaml')
+    def test_parser_holmes_bg_with_missing_elements(self):
+        """
+        Uses manually patched cassette.
+        Verifies:
+            1) Page iteration stops after the first page with no items
+            2) Missing image is not causing failure (this was a bug)
+            3) Missing elements are properly parsed as 'None'
+        """
+        configs = ConfigLoader.load_all_configs(TEST_CONFIG_FOLDER)
+        holmes_cfg = [cfg for cfg in configs if cfg.name == 'test_holmes_bg'][0]
+        parser = Parser(holmes_cfg.sites[0], holmes_cfg.headers)
+
+        items = parser.process()
+        self.assertEqual(1, len(items))
+
+        item = items.values()[0]
+        self.assertIsNone(item.attributes['image'])
+        self.assertTrue('0878426911' in item.attributes['description'])
+        self.assertEquals('http://www.holmes.bg/pcgi/home.cgi?act=3&adv=1j150408486382722', item.link)
+        self.assertEquals('/pcgi/home.cgi?act=3&adv=1j150408486382722', item.key)
